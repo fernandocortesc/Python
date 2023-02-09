e65461a383f00c67baaf0d7e327599f2e45abe8c
fernandocortesc <fernando@cortescalderon.com>
2023-02-09 09:25:49 -0600

    variables

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Python
-Here I'll save the progress of my Python learning
+Here I'll save the progress of my Python learning.
+
+I created this repository to keep a track of my learning path in Python.
+
+I'll be posting my progress, topics and definitions to have a future reference in each topic.
+
+My goal is to post new files and new definitions along with new excercises and their solutions everyday.
---
+++ Variables.py
@@ -0,0 +1,14 @@
+# Variables
+
+# A variable, in short words, is an identifier which can store values. Those values could be strings, math operations, booleans and more.
+
+# Variables are case sensitive, which means "var" and "Var" are not the same. 
+
+# There are some rules when creating a variable:
+# 1. It can contain, lowercases, uppercases, numbers and underscores
+# 2. Hyphens are not allowed
+# 3. Cannot begin with a number
+# 4. The only symbol allowed is "_"
+# 5. Cannot be more than one word: "variable one" -> "variable_one"
+
+# it is possible to reassign variables. This means, it is possible to change the values they previously stored.
