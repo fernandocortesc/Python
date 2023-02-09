25874ecad5cbcecd3d7e9a2f6da94537f6b40ac1
fernandocortesc <fernando@cortescalderon.com>
2023-02-09 10:58:40 -0600

    DataTypes

---
+++ DataTypes.py
@@ -0,0 +1,21 @@
+# Data Types
+
+# Python has different data types
+
+# The most useful data types are:
+# 1. String
+# 2. Numbers
+# 3. Booleans
+# 4. Lists
+# 5. Sets
+# 6. Frozensets
+# 7. Tuples
+# 8. Ranges
+# 9. Dictionaries
+# 10. None
+
+# Some data types can be modified after they were created. This is called mutability and immutability
+
+# Mutable data types are: lists, dictionaries and sets
+
+# Immutable data types are: Strings, numbers, tuples and frozensets
